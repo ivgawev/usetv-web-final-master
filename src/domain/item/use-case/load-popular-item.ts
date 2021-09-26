1560837fafb1e6d529ff4b1178deab82021c8d6f
Iván García <ivgawev@gmail.com>
2021-09-26 14:14:24 +0200

    refactor: use remote-repository contract model

--- src/domain/item/use-case/load-popular-item.ts
+++ src/domain/item/use-case/load-popular-item.ts
@@ -1,15 +1,12 @@
-import { ApiUrlPath } from '@/domain/api'
 import { Language } from '@/domain/language'
 import { Page } from '@/domain/page'
 import { Item } from '@/domain/item'
+import { RemoteRepository } from '@/domain/remote'
 
-export interface LoadPopularItem {
-  load: () => Promise<LoadPopularItem.Response>
-}
+export type LoadPopularItem = RemoteRepository<LoadPopularItem.Params, LoadPopularItem.Response>
 
 export namespace LoadPopularItem {
-  export interface Params {
-    apiUrlPath: ApiUrlPath
+  export interface Params extends RemoteRepository.Params {
     language?: Language
     page?: number
   }
