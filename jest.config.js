825d6e30c269e1e1685543025151c36b245a2923
Iván García <ivgawev@gmail.com>
2021-09-28 18:39:25 +0200

    chore: config testing library

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,26 @@
 module.exports = {
   roots: ['<rootDir>/src', '<rootDir>/tests'],
-  testEnvironment: 'node',
+
   transform: { '.+\\.ts$': 'ts-jest' },
-  testMatch: ['<rootDir>/tests/**/*.(spec|test).ts'],
   moduleNameMapper: {
     '@/tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1'
   },
 
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts']
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+
+  projects: [
+    {
+      displayName: 'dom',
+      testEnvironment: 'jsdom',
+      testMatch: ['<rootDir>/tests/**/*.test.{ts,tsx}'],
+      setupFilesAfterEnv: ['<rootDir>/src/main/config/tests/setup.ts']
+    },
+    {
+      displayName: 'node',
+      testEnvironment: 'node',
+      testMatch: ['<rootDir>/tests/**/*.spec.{ts,tsx}']
+    }
+  ]
 }
