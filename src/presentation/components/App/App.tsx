b21d5a4706d64f6eb218d30ed2d81de8779a4589
Iván García <ivgawev@gmail.com>
2021-09-28 18:56:43 +0200

    feat: add redux provider

--- src/presentation/components/App/App.tsx
+++ src/presentation/components/App/App.tsx
@@ -1,8 +1,10 @@
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { configStore } from '@/presentation/redux'
 import { StrictMode } from 'react'
 
 const App = () => {
-  return <></>
+  return <Provider store={configStore()}></Provider>
 }
 
 render(
