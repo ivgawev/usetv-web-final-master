4c94abe9f4f153dcc6d33021b9dd86ad7b11c18a
Iván García <ivgawev@gmail.com>
2021-09-28 20:08:12 +0200

    feat: add colors foundation

---
+++ src/presentation/theme/foundation/colors/index.ts
@@ -0,0 +1,59 @@
+import {
+  BLUE,
+  CYAN,
+  SKY,
+  TEAL,
+  BLUE_GRAY,
+  COOL_GRAY,
+  GRAY,
+  TRUE_GRAY,
+  WARM_GRAY,
+  EMERALD,
+  GREEN,
+  LIME,
+  ORANGE,
+  FUCHSIA,
+  PINK,
+  ROSE,
+  INDIGO,
+  PURPLE,
+  VIOLET,
+  RED,
+  AMBER,
+  YELLOW
+} from '@/presentation/theme/foundation/colors'
+
+export const Colors = {
+  BLUE,
+  CYAN,
+  SKY,
+  TEAL,
+  BLUE_GRAY,
+  COOL_GRAY,
+  GRAY,
+  TRUE_GRAY,
+  WARM_GRAY,
+  EMERALD,
+  GREEN,
+  LIME,
+  ORANGE,
+  FUCHSIA,
+  PINK,
+  ROSE,
+  INDIGO,
+  PURPLE,
+  VIOLET,
+  RED,
+  AMBER,
+  YELLOW
+} as const
+
+export * from '@/presentation/theme/foundation/colors/blue'
+export * from '@/presentation/theme/foundation/colors/gray'
+export * from '@/presentation/theme/foundation/colors/green'
+export * from '@/presentation/theme/foundation/colors/orange'
+export * from '@/presentation/theme/foundation/colors/pink'
+export * from '@/presentation/theme/foundation/colors/purple'
+export * from '@/presentation/theme/foundation/colors/red'
+export * from '@/presentation/theme/foundation/colors/white'
+export * from '@/presentation/theme/foundation/colors/yellow'
