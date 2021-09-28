245b8d63df3914587522dbdfc1f7c428a4d70e9f
Iván García <ivgawev@gmail.com>
2021-09-28 20:43:21 +0200

    feat: add theme

---
+++ src/presentation/theme/theme.ts
@@ -0,0 +1,8 @@
+import { ThemeConfig } from '@/presentation/theme/interfaces'
+
+export const theme: ThemeConfig = {
+  typography: {
+    defaultFontFamily: 'Poppins',
+    fontFamilies: ['Poppins', 'Lato']
+  }
+}
